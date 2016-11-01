2f59a672c976c7021591930eb84654e65ffe110d
Mike Morris <mike.morris@ecodev.vic.gov.au>
2016-11-02 08:32:43 +1100

    Debugged initial commit

---
+++ bkup_setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from soilmoisture import __version__
+
+setup(
+    name='soilmoisture',
+    version=__version__,
+
+    url='http://host/',
+    author='Mike Morris',
+    author_email='mike.morris@ecodev.vic.gov.au',
+
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=['scripts/manage.py'],
+
+    install_requires=(
+        'django<1.7',
+    )
+)
+
