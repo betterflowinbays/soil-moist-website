d805e77b6134ce2ea316ec77f6352b34966748bc
Mike Morris <mike.morris@ecodev.vic.gov.au>
2016-11-02 08:18:15 +1100

    Initial commit debugged

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ setup(
     version=__version__,
 
     url='http://host/',
-    author='author',
-    author_email='email',
+    author='Mike Morris',
+    author_email='mike.morris@ecodev.vic.gov.au',
 
     packages=find_packages(),
     include_package_data=True,
     scripts=['scripts/manage.py'],
 
     install_requires=(
-        'django<1.7',
+        'django',
     )
 )
 
--- soilmoisture/apps/views.py
+++ soilmoisture/apps/views.py
@@ -4,4 +4,4 @@ def index(request):
     return render(request, 'soilmoisture/home.html')
 
 def contact(request):
-    return render(request, 'soilmoisture/basic.html', {'content':['If you would like to contact me please email me at',' mike.morris@ecodev.vic.gov.au']})
+    return render(request, 'soilmoisture/templates/basic.html', {'content':['If you would like to contact me please email me at',' mike.morris@ecodev.vic.gov.au']})
