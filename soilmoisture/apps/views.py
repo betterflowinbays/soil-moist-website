b945a34ff4739aaf5482fa0b6cffd057950977ba
Mike Morris <mike.morris@ecodev.vic.gov.au>
2016-11-01 15:15:22 +1100

    Initial untried code

---
+++ soilmoisture/apps/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'soilmoisture/home.html')
+
+def contact(request):
+    return render(request, 'soilmoisture/basic.html', {'content':['If you would like to contact me please email me at',' mike.morris@ecodev.vic.gov.au']})
---
+++ soilmoisture/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from .apps import views
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+    url(r'^contact/', views.contact, name='contact'),
+)
+
+# Development
+from django.conf import settings
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    media = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns = media + staticfiles_urlpatterns() + urlpatterns
